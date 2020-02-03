3d98c43f20a68e628b48824f6e82859e205f94b3
quadroturkey <reesemb1@live.com>
2020-02-03 16:53:58 -0600

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,41 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    self.class.all << self        # Better practice than calling @@all, more accurate errors after a failure
+  end
+
+  def galleries 
+    self.paintings.map { |painting| painting.gallery }.uniq
+  end
+
+  def cities
+    self.galleries.map { |gallery| gallery.city }.uniq
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+    self.all.map { |artist| artist.years_experience }.sum
+  end
+
+  def self.most_prolific
+    all.max_by { |artist| artist.paintings.count / artist.years_experience }
+  end
+
+  def create_painting(title, price, gallery)
+    new_painting = Painting.new(title, price, self, gallery)
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,25 @@
 class Painting
 
   attr_reader :title, :price
+  attr_accessor :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    self.all.map { |painting| painting.price }.sum
   end
 
 end
